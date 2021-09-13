3994831791a3f816f207b49eef37ffc5a1b7dfa7
cristina <dominguez.ac@gmail.com>
2021-09-13 18:29:15 +0200

    Ordenar listado de experiencias

--- controladores/experiencias/listExperiencias.js
+++ controladores/experiencias/listExperiencias.js
@@ -7,7 +7,24 @@ const listExperiencias = async (req, res, next) => {
     connection = await getDB();
 
     //saco el filtro de la busqueda req.query
-    const { search } = req.query;
+    const { search, orden, direccion } = req.query;
+    // definimos los valores de orden y direccion
+    const validOrden = [
+      "titulo",
+      "descripcion",
+      "localidad",
+      "n_plazas",
+      "f_inicio",
+      "precio",
+      "votos",
+    ];
+
+    const ordenBy = validOrden.includes(orden) ? orden : "votos";
+
+    const validDireccion = ["ASC", "DESC"];
+    const ordenDireccion = validDireccion.includes(direccion)
+      ? direccion
+      : "DESC";
 
     let result;
 
@@ -20,6 +37,7 @@ const listExperiencias = async (req, res, next) => {
             LEFT JOIN experiencia_puntuacion ON (experiencia.id = experiencia_puntuacion.experiencia_id)
             WHERE experiencia.titulo LIKE ? OR experiencia.localidad LIKE ? 
             GROUP BY experiencia.id
+            ORDER BY ${ordenBy} ${ordenDireccion}
             `,
         [`%${search}%`, `%${search}%`]
       );
@@ -30,6 +48,7 @@ const listExperiencias = async (req, res, next) => {
       FROM experiencia
       LEFT JOIN experiencia_puntuacion ON (experiencia.id = experiencia_puntuacion.experiencia_id)
       GROUP BY experiencia.id
+      ORDER BY ${ordenBy} ${ordenDireccion}
 
       `);
     }
